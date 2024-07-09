1d947b590644519fef9eb07e0e4205ed1152bc77
Matthias Wolter <woltermatthias199@gmail.com>
2024-07-09 22:45:21 +0200

    Exception Classes hihi haha

---
+++ stock_mechanics/include/exception_classes.hpp
@@ -0,0 +1,47 @@
+#include <exception>
+#include <string>
+namespace ProjectStockMarket{
+class UserNotFound : public std::exception{
+    public:
+    UserNotFound(const std::string s);
+    private:
+    std::string m_message;
+};
+
+class ProductNotFound : public std::exception{
+    public:
+    ProductNotFound(const std::string s);
+    private:
+    std::string m_message;
+};
+
+class NotEnoughMoney : public std::exception{
+    public:
+    NotEnoughMoney(const std::string s);
+    private:
+    std::string m_message;
+};
+
+class OutOfStock : public std::exception{
+    public:
+    OutOfStock(const std::string s);
+    private:
+    std::string m_message;
+};
+
+class IncorrectPassword : public std::exception{
+    public:
+    IncorrectPassword(const std::string s);
+    private:
+    std::string m_message;
+};
+
+class InvalidToken : public std::exception{
+    public:
+    InvalidToken(const std::string s);
+    private:
+    std::string m_message;
+};
+
+
+}
---
+++ stock_mechanics/src/exception_classes.cpp
@@ -0,0 +1,25 @@
+#include "exception_classes.hpp"
+
+using namespace ProjectStockMarket;
+
+UserNotFound::UserNotFound(const std::string p)  :  m_message(p){}
+
+ProjectStockMarket::ProductNotFound::ProductNotFound(const std::string p)  :  m_message(p)
+{
+}
+
+ProjectStockMarket::NotEnoughMoney::NotEnoughMoney(const std::string p)  :  m_message(p)
+{
+}
+
+ProjectStockMarket::OutOfStock::OutOfStock(const std::string p)  :  m_message(p)
+{
+}
+
+ProjectStockMarket::IncorrectPassword::IncorrectPassword(const std::string p)  :  m_message(p)
+{
+}
+
+ProjectStockMarket::InvalidToken::InvalidToken(const std::string p)  :  m_message(p)
+{
+}
